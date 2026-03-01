60d2d6a822d659c96534ba8cfeb3ad429b90ebd5
mahim0017 <mahimtonmoy0190@gmail.com>
2026-03-02 03:54:38 +0600

    medium designed

--- script/script.js
+++ script/script.js
@@ -8,19 +8,21 @@ const products = [
     { name: "Zenfone 10", price: 55000, brand: "Asus" },
     { name: "Moto Edge 40", price: 40000, brand: "Motorola" },
     { name: "V30 Pro", price: 45000, brand: "Vivo" },
-    { name: "Reno 11", price: 38000, brand: "Oppo" }
+    { name: "Reno 11", price: 38000, brand: "Oppo" },
+    { name: "Nothing Phone 2", price: 60000, brand: "Nothing" },
+    { name: "Realme GT 5", price: 42000, brand: "Realme" }
 ];
 
 let cart = [];
+const productParent = document.querySelector("#productParent");
 
 function renderApp() {
-    const productParent = document.querySelector("#productParent");
     productParent.innerHTML = ""; 
 
     for (const item of products) {
         const card = document.createElement("div");
 
-        card.classList.add("border-2", "border-black", "p-4", "m-4", "w-64");
+        card.classList.add( "bg-white","rounded-2xl","shadow-lg","p-5","m-4","w-72","transition","duration-300","hover:scale-105","hover:shadow-2xl");
 
         const h1 = document.createElement("h1");
 
@@ -28,14 +30,14 @@ function renderApp() {
         h1.classList.add("font-bold", "text-xl");
 
         const h2 = document.createElement("h2");
-        h2.textContent = "Brand";
+        h2.textContent = item.brand;
 
         const p = document.createElement("p");
-        p.textContent = "Price";
+        p.textContent = item.price;
 
         const btn = document.createElement("button");
         btn.textContent = "add to cart";
-        btn.classList.add("border-2", "border-black", "px-2", "py-1", "mt-2", "bg-gray-100","hover:bg-black", "hover:text-white", "cursor-pointer");
+        btn.classList.add("w-full","bg-black","text-white","py-2","rounded-xl","transition","duration-300","hover:bg-gray-800","cursor-pointer");
 
         btn.addEventListener("click", function() {
             cart.push(item);
